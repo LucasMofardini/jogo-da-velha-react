bc871029352eb43823798592682ccf1782fd2757
Lucas Mofardini <lucasmofardini@gmail.com>
2022-02-14 22:31:08 -0300

    Restei o jogo construindo com MAP

--- src/components/EstruturaJogoDaVelha.js
+++ src/components/EstruturaJogoDaVelha.js
@@ -4,54 +4,44 @@ import '../assets/css/JogoDaVelha.css';
 export default function EstruturaJogoDaVelha() {
     const xis = 'x';
     const bolinha = 'o';
+    //  
+    const rodadaInicial = Array(9).fill("");
     const [jogadorAtivo, setJogadorAtivo] = useState(xis);
-    const [rodada, setRodada] = useState([]);
+    const [rodada, setRodada] = useState(rodadaInicial);
+
 
     useEffect(() => {
+        console.log(rodada)
+    })
 
+    function logicaClick(event, index) {
+        console.log(event);
 
-        console.log(rodada);
+        setRodada(
+            rodada.map((item, itemIndex, key) => {
+                console.log(' item : ', index);
+                console.log('item index : ', itemIndex);
+                console.log('key ', key)
+                // return itemIndex === index ? jogadorAtivo : item
+                if (itemIndex === index) {
 
+                    // ele so pode setar se o jogador nao for o mesmo
+                    return (jogadorAtivo);
 
+                } else {
+                    return (item);
+                }
+            })
+        );
 
-    });
-
-    function logicaClick(event) {
-        event.preventDefault();
-        selecionarNoJogo(event);
-
-    }
-    function selecionarNoJogo(event) {
-        /* A função precisa : 
-            - Setar a o click na rodada,
-            - Atualizar a rodada,
-            - Mudar o jogador,
-        */
-        /* Toda vez que voce clica, ele PEGA o Aria-Label e pega o Jogador ativo. 
-        Eu preciso fazer > Depois de clicado ele Seta A rodada e vai colocar o aria-label e jogador ativo no seu lugar  */
-        let ariaLabel = +event.target.ariaLabel;
-
-        setRodada([
-            ...rodada,
-            { jogadorAtivo, ariaLabel }
-        ]);
-        atualizaRodada(rodada);
 
         /* Depois muda o jogador */
-
-        mudarOJogador(jogadorAtivo);
-
-
-    }
-    function atualizaRodada(rodada) {
-        // event.target.value = jogadorAtivo;
-        // event.target.innerText = jogadorAtivo;
-
-        // Precisa pegar a rodada e atualizar nos btn
+        mudarOJogador();
 
 
     }
-    function mudarOJogador(jogadorAtivo) {
+
+    function mudarOJogador() {
         // console.log(jogadorAtivo);
 
         if (jogadorAtivo === xis) {
@@ -63,11 +53,12 @@ export default function EstruturaJogoDaVelha() {
 
     }
     function resetarOJogo() {
-        setRodada([]);
-        atualizaRodada();
+        setRodada(rodadaInicial);
     }
     return (
+
         <div className='container-all'>
+
             <div className='container-jogador'>
                 <h2>
                     Vez de jogar :
@@ -77,22 +68,24 @@ export default function EstruturaJogoDaVelha() {
             </div>
             <div className="container-body-jogo">
                 <div className='container-jogo'>
-                    <div className='linha-btn' id="linha-1">
-                        <button onClick={logicaClick} value="" aria-label='1'> </button>
-                        <button onClick={logicaClick} value="" aria-label='2'> </button>
-                        <button onClick={logicaClick} value="" aria-label='3'> </button>
-                    </div>
-                    <div className='linha-btn' id="linha-2">
-                        <button onClick={logicaClick} value="" aria-label='4'> </button>
-                        <button onClick={logicaClick} value="" aria-label='5'> </button>
-                        <button onClick={logicaClick} value="" aria-label='6'> </button>
-                    </div>
-                    <div className='linha-btn' id="linha-3">
-                        <button onClick={logicaClick} value="" aria-label='7'> </button>
-                        <button onClick={logicaClick} value="" aria-label='8'> </button>
-                        <button onClick={logicaClick} value="" aria-label='9'> </button>
+                    <div className='linha-btn'>
+                        {
+                            rodada.map((item, index) => {
+                                return (
+                                    <button
+                                        className={`${item} ${index}`}
+                                        onClick={() => {
+                                            logicaClick(index)
+                                        }}
+                                        key={index}>
+                                        {item}
+                                    </button>
+                                );
+                            })
+                        }
                     </div>
                 </div>
+
                 <button onClick={resetarOJogo}> Resetar </button>
 
             </div>
