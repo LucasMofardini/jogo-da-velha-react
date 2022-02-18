77452740f3e0fec39f3a56ff0900c3b01a6261ff
Lucas Mofardini <lucasmofardini@gmail.com>
2022-02-18 19:34:42 -0300

    Jogo da velha finalizado

--- src/components/EstruturaJogoDaVelha.js
+++ src/components/EstruturaJogoDaVelha.js
@@ -9,29 +9,25 @@ export default function EstruturaJogoDaVelha() {
     const [jogadorAtivo, setJogadorAtivo] = useState(xis);
     const [vencedor, setJogadorVencedor] = useState();
     const [rodada, setRodada] = useState(rodadaInicial);
-
+    const [contadorRodada, setContadorRodada] = useState(0);
 
     useEffect(verificaGanhador, [rodada]);
 
     function logicaClick(index) {
         console.log(index);
-
+        // console.log(rodada);
         if (rodada[index] !== "") {
             return null;
         }
         if (vencedor) {
             return null;
         }
+        setContadorRodada(contadorRodada + 1)
         setRodada(
             rodada.map((item, itemIndex) => {
                 // return itemIndex === index ? jogadorAtivo : item
                 if (itemIndex === index) {
-                    // console.log(' item : ', index);
-                    // console.log('item index : ', itemIndex);
-
                     return (jogadorAtivo);
-
-
                 } else {
                     return (item);
                 }
@@ -58,20 +54,31 @@ export default function EstruturaJogoDaVelha() {
             //Diagonais
             [rodada[0], rodada[4], rodada[8]],
             [rodada[2], rodada[4], rodada[6]],
+            //Velha
+            // [rodada[0], rodada[1], rodada[2],
+            // rodada[3], rodada[4], rodada[5],
+            // rodada[6], rodada[7], rodada[8]]
         ];
 
         arrayFormasDeVencer.forEach((formasDeVencer) => {
             if (formasDeVencer.every(forma => forma === bolinha)) {
                 setJogadorVencedor(bolinha);
+                console.log(formasDeVencer)
             }
             if (formasDeVencer.every(forma => forma === xis)) {
                 setJogadorVencedor(xis);
-
+                console.log(formasDeVencer)
             }
+            checkEmpate();
         });
+
+    }
+    function checkEmpate() {
+        if (rodada.every(item => item !== "")) {
+            setJogadorVencedor("Empate");
+        }
     }
     function mudarOJogador() {
-        // console.log(jogadorAtivo);
 
         if (jogadorAtivo === xis) {
             setJogadorAtivo(bolinha);
@@ -83,17 +90,36 @@ export default function EstruturaJogoDaVelha() {
     }
     function resetarOJogo() {
         setRodada(rodadaInicial);
+        if (vencedor == "Empate") {
+            setJogadorAtivo(xis);
+
+        } else {
+            setJogadorAtivo(vencedor);
+
+        }
         setJogadorVencedor();
+
     }
     return (
 
         <div className='container-all'>
 
             <div className='container-jogador'>
-                <h2>
-                    {vencedor ? `O Vencedor é : ` : 'Vez de jogar :'}
-                </h2>
-                <span> {vencedor ? vencedor.toLocaleUpperCase() : jogadorAtivo.toLocaleUpperCase()}</span>
+                {vencedor == "Empate" ?
+                    <div>
+                        <h2>
+                            Deu Velha
+                        </h2>
+                    </div>
+                    :
+                    <>
+                        <h2>
+                            {vencedor ? `O Vencedor é : ` : 'Vez de jogar :'}
+                        </h2>
+                        <span> {vencedor ? vencedor.toLocaleUpperCase() : jogadorAtivo.toLocaleUpperCase()}</span>
+                    </>
+                }
+
 
                 <div className="container-botao-resetar">
                     {vencedor ? <button onClick={resetarOJogo}> Resetar Partida </button> : ""}
